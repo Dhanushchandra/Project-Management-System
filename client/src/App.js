2f9b662cdab58696d0002fa754b56d5bb7a1ddc6
Dhanushchandra <Dhanushbharath9@gmail.com>
2023-08-26 17:59:10 +0530

    configured for env

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import AddClientModal from "./components/AddClientModal";
-import Clients from "./components/Clients";
 import Header from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
-import Projects from "./components/Projects";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Project from "./pages/Project";
@@ -28,7 +25,7 @@ const cache = new InMemoryCache({
 });
 
 const client = new ApolloClient({
-  uri: "http://localhost:5000/graphql",
+  uri: process.env.REACT_APP_GRAPHQL_KEY,
   cache,
 });
 
